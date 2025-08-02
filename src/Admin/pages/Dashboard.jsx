44addfeb68f96515a3be01da6698517ad2c4aaf1
Collid3 <masolathabangjerry11@gmail.com>
2025-08-02 16:30:54 +0200

    added dashboard custom barchart to fully complete dashboard page

--- src/Admin/pages/Dashboard.jsx
+++ src/Admin/pages/Dashboard.jsx
@@ -10,7 +10,8 @@ import { addThousandsSeparator } from "../../utils/helper";
 import InfoCard from "../components/InfoCard";
 import { LuArrowRight } from "react-icons/lu";
 import TaskListTable from "../components/TaskListTable";
-import CustomPieChart from "../components/PieChart/CustomPieChart";
+import CustomPieChart from "../components/Charts/CustomPieChart";
+import CustomBarChart from "../components/Charts/CustomBarChart";
 
 const COLORS = ["#8D51FF", "#00BBDB", "#7BCE00"];
 
@@ -121,6 +122,16 @@ const Dashboard = () => {
               </div>
 
               <CustomPieChart data={pieChartData} colors={COLORS} />
+            </div>
+          </div>
+
+          <div>
+            <div className="card">
+              <div className="flex items-center justify-between">
+                <h5>Task Priority Levels</h5>
+              </div>
+
+              <CustomBarChart data={barChartData} />
             </div>
           </div>
 
